6e885f78496cb59cc50e7b1e3ba445956a72e37a
yanzhe-Xiao <2397903439@qq.com>
2025-11-19 20:57:42 +0800

    1、2

---
+++ 1.两数之和.py
@@ -0,0 +1,22 @@
+# @before-stub-for-debug-begin
+from python3problem1 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # print(1)
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        return []
+# @lc code=end
+
